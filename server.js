d99d209b5c33da18457c054e5969f1d799e4693e
Samuel Encinas <samuel.encinasplaza@gmail.com>
2021-09-16 09:50:55 +0200

    h

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const secretMongoDB = process.env.MONGODB_URI;
 
 // Middlewares
 app.use(bodyParser.json());
-app.use(cors({credentials: true, origin: 'http://localhost:8080'}))
+app.use(cors({credentials: true, origin: 'https://ecodium.dev'}))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use('/subidas', express.static('uploads'));
