edd40e50182dd3534e5df9af8b8ba893183165c1
Samuel Encinas <samuel.encinasplaza@gmail.com>
2021-09-16 03:22:15 +0200

    b

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -11,7 +11,7 @@ router.get("/google", passport.authenticate("google", {
 // Callback 
 router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
     const user = req.user;
-    return res.status(200).send('<script>window.opener.location="https://localhost:8080"; self.close();</script>');
+    return res.status(200).send('<script>window.opener.location="http://localhost:8080"; self.close();</script>');
     
 });
 
